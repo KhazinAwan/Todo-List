a664fff454a06fac48223feb55d223b46686603e
Khazin <175429319+KhazinAwan@users.noreply.github.com>
2026-07-12 16:11:59 +0500

    Fix a few layout issues

--- src/components/todoContainer/todoContainer.js
+++ src/components/todoContainer/todoContainer.js
@@ -5,21 +5,28 @@ function createTodoContainer() {
     const todoContainer = document.createElement("main");
     todoContainer.classList.add("todoContainer");
 
+    const todoHeader = document.createElement("div");
+    todoHeader.classList.add("todoHeader");
+
     const heading = document.createElement("h2");
     heading.classList.add("todoContainerHeading");
     heading.textContent = "Todos";
-
-    const todoList = document.createElement("div");
-    todoList.classList.add("todoList");
 
     const addTodoButton = document.createElement("button");
     addTodoButton.classList.add("addTodoButton");
     addTodoButton.textContent = "+ New Todo";
 
-    todoContainer.append(
+    todoHeader.append(
         heading,
-        todoList,
         addTodoButton
+    );
+
+    const todoList = document.createElement("div");
+    todoList.classList.add("todoList");
+
+    todoContainer.append(
+        todoHeader,
+        todoList
     );
 
     return todoContainer;
