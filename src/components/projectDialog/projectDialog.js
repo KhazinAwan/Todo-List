58923929c71341c786e0c61bc10446e0cd3175cd
Khazin <175429319+KhazinAwan@users.noreply.github.com>
2026-07-12 15:27:46 +0500

    Create projectDialog component

---
+++ src/components/projectDialog/projectDialog.js
@@ -0,0 +1,50 @@
+import "./projectDialog.css";
+
+function createProjectDialog() {
+
+    const dialog = document.createElement("dialog");
+    dialog.classList.add("projectDialog");
+
+    const heading = document.createElement("h2");
+    heading.textContent = "Project";
+
+    const form = document.createElement("form");
+
+    const nameLabel = document.createElement("label");
+    nameLabel.textContent = "Project Name";
+
+    const nameInput = document.createElement("input");
+    nameInput.type = "text";
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("dialogButtons");
+
+    const saveButton = document.createElement("button");
+    saveButton.type = "submit";
+    saveButton.textContent = "Save";
+
+    const cancelButton = document.createElement("button");
+    cancelButton.type = "button";
+    cancelButton.textContent = "Cancel";
+
+    buttonContainer.append(
+        saveButton,
+        cancelButton
+    );
+
+    form.append(
+        nameLabel,
+        nameInput,
+        buttonContainer
+    );
+
+    dialog.append(
+        heading,
+        form
+    );
+
+    return dialog;
+
+}
+
+export default createProjectDialog;
