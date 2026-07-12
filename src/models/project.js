fe16dc00bad90c41fca73cca717ea09665366b2c
Khazin <175429319+KhazinAwan@users.noreply.github.com>
2026-07-12 16:49:14 +0500

    Set up todo and project models

---
+++ src/models/project.js
@@ -0,0 +1,24 @@
+class Project {
+
+    constructor(name) {
+
+        this.name = name;
+        this.todos = [];
+
+    }
+
+    addTodo(todo) {
+
+        this.todos.push(todo);
+
+    }
+
+    removeTodo(todoIndex) {
+
+        this.todos.splice(todoIndex, 1);
+
+    }
+
+}
+
+export default Project;
