9773dbbf6ed76658d529cc04ca52abe1aebbd9a7
Khazin <175429319+KhazinAwan@users.noreply.github.com>
2026-07-13 00:37:23 +0500

    Check rendering of default project and default todo

--- src/appController.js
+++ src/appController.js
@@ -1,12 +1,51 @@
 import createApp from "./App.js";
+import appState from "./state/appState.js";
+import Project from "./models/project.js";
+import Todo from "./models/todo.js";
+
+import { renderProjects } from "./components/projectSidebar/projectSidebarView.js";
+import { renderTodos } from "./components/todoContainer/todoContainerView.js";
+
+let ui;
+
+function initializeDefaultProject() {
+
+    const defaultProject = new Project("Default");
+
+    const defaultTodo = new Todo(
+        "Welcome to your Todo List!",
+        "Create a new project or add a new todo to get started.",
+        "2026-07-31",
+        "Medium"
+    );
+
+    defaultProject.addTodo(defaultTodo);
+
+    appState.projects.push(defaultProject);
+
+    appState.currentProject = defaultProject;
+
+}
 
 function initialize() {
 
     const app = document.getElementById("app");
 
-    const ui = createApp();
+    ui = createApp();
 
     app.append(ui.root);
+
+    initializeDefaultProject();
+
+    renderProjects(
+        ui.projectList,
+        appState.projects
+    );
+
+    renderTodos(
+        ui.todoList,
+        appState.currentProject.todos
+    );
 
 }
 
